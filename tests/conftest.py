2467c8789f4731ab9355e7697b8124150edc1d6f
Alison Americo <alisonlira@proton.me>
2026-03-24 20:36:08 -0300

    add first migration with Alembic. Create users table.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import Session
 
 from fast_zero.app import app, database
+from fast_zero.models import table_registry
 from fast_zero.schemas import UserDB
 
 
@@ -26,3 +32,34 @@ def client_with_user():
         )
     )
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
+    engine.dispose()
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2026, 1, 1)):
+
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
