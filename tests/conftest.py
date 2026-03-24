494ec0708797ade104c39f48a1a638d1e759cb51
Alison Americo <alisonlira@proton.me>
2026-03-24 20:54:21 -0300

    add updated_at field in users table

--- tests/conftest.py
+++ tests/conftest.py
@@ -52,6 +52,7 @@ def _mock_db_time(*, model, time=datetime(2026, 1, 1)):
     def fake_time_hook(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
+            target.updated_at = time
 
     event.listen(model, 'before_insert', fake_time_hook)
 
